ee4f44a081859c220c73a2a5de92bc8c69ce089c
Krzysztof Kalisz <kalisz.krzysztofl@gmail.com>
2022-11-25 11:28:39 +0100

    Init

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,7 +12,11 @@ const Menu = ({ children }) => {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
             </label>
           </div>
-          <div className="flex-1 px-2 mx-2">react-query-demo</div>
+          <div className="flex-1 px-2 mx-2">
+            <Link href="/">
+              react-query-demo
+            </Link>
+          </div>
           <div className="flex-none hidden lg:block">
             <ul className="menu menu-horizontal">
               <li>
